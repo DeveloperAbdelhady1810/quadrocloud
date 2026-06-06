52554aa711e3944ce398da12b0e13aa87c206f1d
Ahmed Abd El Hady <ahmed.mohamed.ahady@gmail.com>
2026-06-06 19:18:44 +0300

    latest

--- mobile/lib/core/constants/app_constants.dart
+++ mobile/lib/core/constants/app_constants.dart
@@ -1,5 +1,6 @@
 class AppConstants {
-  static const String baseUrl = 'http://192.168.1.4:8080/api/v1';
+  // static const String baseUrl = 'http://192.168.1.4:8080/api/v1';
+  static const String baseUrl = 'https://management.quadrocloud.net/api/v1';
   static const String tokenKey = 'auth_token';
   static const String clientKey = 'client_data';
   static const String localeKey = 'app_locale';
